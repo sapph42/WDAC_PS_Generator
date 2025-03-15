7b082e10823d5206ad5e4a4a950156410b38cce1
sapph42 <31635854+sapph42@users.noreply.github.com>
2025-03-15 03:43:53 -0400

    Resize form elements

--- WDAC_PS_Generator/Forms/Main.Designer.cs
+++ WDAC_PS_Generator/Forms/Main.Designer.cs
@@ -59,7 +59,7 @@
             // NewButton
             // 
             NewButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            NewButton.Location = new Point(6, 134);
+            NewButton.Location = new Point(6, 100);
             NewButton.Margin = new Padding(6);
             NewButton.Name = "NewButton";
             NewButton.Size = new Size(470, 156);
@@ -71,7 +71,7 @@
             // AddButton
             // 
             AddButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            AddButton.Location = new Point(971, 134);
+            AddButton.Location = new Point(971, 100);
             AddButton.Margin = new Padding(6);
             AddButton.Name = "AddButton";
             AddButton.Size = new Size(472, 156);
@@ -103,14 +103,14 @@
             tableLayoutPanel1.Margin = new Padding(6);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 7;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 128F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 45F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 85F));
-            tableLayoutPanel1.Size = new Size(1449, 823);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(1449, 546);
             tableLayoutPanel1.TabIndex = 4;
             // 
             // GenerateXml
@@ -119,7 +119,7 @@
             GenerateXml.AutoSize = true;
             GenerateXml.Checked = true;
             GenerateXml.CheckState = CheckState.Checked;
-            GenerateXml.Location = new Point(603, 747);
+            GenerateXml.Location = new Point(603, 495);
             GenerateXml.Name = "GenerateXml";
             GenerateXml.Size = new Size(241, 36);
             GenerateXml.TabIndex = 5;
@@ -129,30 +129,31 @@
             // 
             // label4
             // 
-            label4.Location = new Point(971, 441);
+            label4.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            label4.Location = new Point(971, 394);
             label4.Margin = new Padding(6, 0, 6, 0);
             label4.Name = "label4";
-            label4.Size = new Size(470, 267);
+            label4.Size = new Size(472, 83);
             label4.TabIndex = 6;
             label4.Text = "Use this to add a new CA to an existing WDAC file";
             label4.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // label3
             // 
-            label3.Location = new Point(488, 441);
+            label3.Location = new Point(488, 390);
             label3.Margin = new Padding(6, 0, 6, 0);
             label3.Name = "label3";
-            label3.Size = new Size(470, 267);
+            label3.Size = new Size(470, 91);
             label3.TabIndex = 6;
             label3.Text = "Use this to replace a single CA in an existing WDAC file with a new one.";
             label3.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
-            label2.Location = new Point(6, 441);
+            label2.Location = new Point(6, 390);
             label2.Margin = new Padding(6, 0, 6, 0);
             label2.Name = "label2";
-            label2.Size = new Size(470, 267);
+            label2.Size = new Size(470, 91);
             label2.TabIndex = 5;
             label2.Text = "Use this to generate a new WDAC file with selected trusted CAs.";
             label2.TextAlign = ContentAlignment.MiddleLeft;
@@ -160,7 +161,7 @@
             // ReplaceButton
             // 
             ReplaceButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            ReplaceButton.Location = new Point(488, 134);
+            ReplaceButton.Location = new Point(488, 100);
             ReplaceButton.Margin = new Padding(6);
             ReplaceButton.Name = "ReplaceButton";
             ReplaceButton.Size = new Size(471, 156);
@@ -175,7 +176,7 @@
             SmartCardCheck.AutoSize = true;
             SmartCardCheck.Checked = true;
             SmartCardCheck.CheckState = CheckState.Checked;
-            SmartCardCheck.Location = new Point(49, 747);
+            SmartCardCheck.Location = new Point(49, 495);
             SmartCardCheck.Margin = new Padding(6);
             SmartCardCheck.Name = "SmartCardCheck";
             SmartCardCheck.Size = new Size(383, 36);
@@ -187,7 +188,7 @@
             // 
             CaLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             CaLabel.AutoSize = true;
-            CaLabel.Location = new Point(485, 314);
+            CaLabel.Location = new Point(485, 262);
             CaLabel.Name = "CaLabel";
             CaLabel.Size = new Size(236, 32);
             CaLabel.TabIndex = 5;
@@ -198,7 +199,7 @@
             // 
             CaSelection.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             CaSelection.FormattingEnabled = true;
-            CaSelection.Location = new Point(485, 349);
+            CaSelection.Location = new Point(485, 297);
             CaSelection.Name = "CaSelection";
             CaSelection.Size = new Size(477, 40);
             CaSelection.TabIndex = 6;
@@ -207,7 +208,7 @@
             // ExecuteReplace
             // 
             ExecuteReplace.Anchor = AnchorStyles.Top;
-            ExecuteReplace.Location = new Point(648, 394);
+            ExecuteReplace.Location = new Point(648, 343);
             ExecuteReplace.Name = "ExecuteReplace";
             ExecuteReplace.Size = new Size(150, 44);
             ExecuteReplace.TabIndex = 5;
@@ -222,7 +223,7 @@
             GenerateP7b.AutoSize = true;
             GenerateP7b.Checked = true;
             GenerateP7b.CheckState = CheckState.Checked;
-            GenerateP7b.Location = new Point(1090, 747);
+            GenerateP7b.Location = new Point(1090, 495);
             GenerateP7b.Name = "GenerateP7b";
             GenerateP7b.Size = new Size(234, 36);
             GenerateP7b.TabIndex = 8;
@@ -234,7 +235,7 @@
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1493, 959);
+            ClientSize = new Size(1493, 596);
             Controls.Add(tableLayoutPanel1);
             Margin = new Padding(6);
             Name = "Main";
